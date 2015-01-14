d6f36048e7f9ef8a63717539a0ef8858e381256b
Sophie POCHEZ <spochez@e2r4p12.42.fr>
2015-01-14 01:22:01 +0100

    up

--- srcs/ft_dec_to_base.c
+++ srcs/ft_dec_to_base.c
@@ -6,31 +6,25 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/13 04:17:14 by spochez           #+#    #+#             */
-/*   Updated: 2015/01/14 00:59:32 by spochez          ###   ########.fr       */
+/*   Updated: 2015/01/14 01:17:44 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_conv_dec_bin(int dec, int base)
+char	*ft_conv_dec_base(int dec, int base)
 {
-	char	*tp;
-	char	*bin;
-	int		i;
-	int		len;
+	char		*bin;
+	intmax_t	tp;
 
-	i = 0;
-	tp = ft_itoa(dec);
-	len = ft_strlen(tp);
-	bin = (char *)malloc(sizeof(char) * (len * 3) + 1);
 	while (dec > 0)
 	{
-		bin[i] = (dec % base) + '0';
+		tp = (dec % base) + '0';
 		dec /= base;
-		i++;
+		if (dec > 0)
+			tp *= 10;
 	}
-	bin[i] = 0;
-	bin = ft_strrev(bin);
+	bin = ft_itoa(tp);
 	return (bin);
 }
