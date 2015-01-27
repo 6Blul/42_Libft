3f08749e60a890e5db8c4652d90418f8acf5f667
Sophie POCHEZ <spochez@e2r3p12.42.fr>
2015-01-27 12:10:32 +0100

    Reglage dec to base

--- srcs/ft_dec_to_base.c
+++ srcs/ft_dec_to_base.c
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/13 04:17:14 by spochez           #+#    #+#             */
-/*   Updated: 2015/01/14 01:17:44 by spochez          ###   ########.fr       */
+/*   Updated: 2015/01/27 12:10:12 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ char	*ft_conv_dec_base(int dec, int base)
 	char		*bin;
 	intmax_t	tp;
 
+	tp = 0;
 	while (dec > 0)
 	{
-		tp = (dec % base) + '0';
+		tp += (dec % base);
 		dec /= base;
 		if (dec > 0)
 			tp *= 10;
 	}
 	bin = ft_itoa(tp);
+	bin = ft_strrev(bin);
 	return (bin);
 }
