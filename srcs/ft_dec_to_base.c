4e8cf2e9ce25205deeeaddb4e3d7c51b73c38d00
Sophie POCHEZ <spochez@e2r3p10.42.fr>
2015-02-03 06:49:45 +0100

    oktamer

--- srcs/ft_dec_to_base.c
+++ srcs/ft_dec_to_base.c
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/12 02:37:38 by spochez           #+#    #+#             */
-/*   Updated: 2015/02/03 06:48:54 by spochez          ###   ########.fr       */
+/*   Updated: 2015/02/03 06:49:41 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_complete(char *bin)
 {
 	int		len;
 	char	*tp;
-	char 	*ret;
+	char	*ret;
 
 	len = ft_strlen(bin) + 1;
 	ret = (char *)malloc(sizeof(char) * len + 1);
@@ -34,7 +34,7 @@ char	*ft_complete(char *bin)
 	return (ret);
 }
 
-char	*ft_conv_dec_base(int dec, int base)	
+char	*ft_conv_dec_base(int dec, int base)
 {
 	char		*bin;
 	uintmax_t	tp;
